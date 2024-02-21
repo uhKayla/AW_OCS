225bcf4da05e4180bdf53330c3e5a9580b88e110
uhKayla <kaylaa@asia.com>
2024-02-21 15:09:20 -0500

    Penetrators, OCS, contacts, and more..

--- Runtime/AW_OCS.cs
+++ Runtime/AW_OCS.cs
@@ -8,13 +8,13 @@ namespace ANGELWARE.AW_OCS
     [Serializable]
     public class AW_OCS : MonoBehaviour, IEditorOnly
     {
-        public bool nsfwTouch;
-        public Transform nsfwTouchTransform;
-        public float nsfwTouchRadius;
-        
-        public bool dickTouch;
-        public Transform dickTouchTransform;
-        public float dickTouchRadius;
+        // public bool nsfwTouch;
+        // public Transform nsfwTouchTransform;
+        // public float nsfwTouchRadius;
+
+        // public bool dickTouch;
+        // public Transform dickTouchTransform;
+        // public float dickTouchRadius;
     }
 }
 #endif
--- Runtime/AW_OCSPlugin.cs
+++ Runtime/AW_OCSPlugin.cs
@@ -27,15 +27,15 @@ namespace ANGELWARE.AW_OCS
                     _contactReceiver = new AW_ContactReceiver();
                     _contactSender = new AW_ContactSender();
                     
-                    var worldGameObject = new GameObject("World");
+                    // var worldGameObject = new GameObject("World");
                     var rootGameObject = new GameObject("OCS_Root");
-                    rootGameObject.transform.SetParent(_component.transform);
+                    rootGameObject.transform.SetParent(_component.transform, true);
                 
-                    var senders = CreateSenders(worldGameObject);
-                    senders.transform.SetParent(rootGameObject.transform);
+                    var senders = CreateSenders(rootGameObject);
+                    senders.transform.SetParent(rootGameObject.transform, true);
 
-                    var receiver = CreateReceivers(worldGameObject);
-                    receiver.transform.SetParent(rootGameObject.transform);
+                    var receiver = CreateReceivers(rootGameObject);
+                    receiver.transform.SetParent(rootGameObject.transform, true);
                 });
         }
 
@@ -46,38 +46,43 @@ namespace ANGELWARE.AW_OCS
             var versionObject = new GameObject("OCS_VersionBeacon");
             _contactSender.CreateContactSender(versionObject, null, 0, 0.01f, Vector3.zero, Vector3.zero,
                 new List<string> { "OCS_V1" });
-            versionObject.transform.SetParent(sendersObject.transform, false);
+            versionObject.transform.SetParent(sendersObject.transform, true);
 
             var optionObjects = new GameObject("OCS_Senders");
             
-            optionObjects.transform.SetParent(sendersObject.transform, false);
+            optionObjects.transform.SetParent(sendersObject.transform, true);
             
-            var opt1Object = new GameObject("OCS_Option1Sender");
-            var opt2Object = new GameObject("OCS_Option2Sender");
-            var opt3Object = new GameObject("OCS_Option3Sender");
-            var optFinishObject = new GameObject("OCS_FinishSender");
+            var opt0Object = new GameObject("OCS0_Sender");
+            var opt1Object = new GameObject("OCS1_Sender");
+            var opt2Object = new GameObject("OCS2_Sender");
+            var opt3Object = new GameObject("OCS3_Sender");
+            var optFinishObject = new GameObject("OCSF_Sender");
             
-            opt1Object.transform.SetParent(optionObjects.transform, false);
-            opt2Object.transform.SetParent(optionObjects.transform, false);
-            opt3Object.transform.SetParent(optionObjects.transform, false);
-            optFinishObject.transform.SetParent(optionObjects.transform, false);
+            opt0Object.transform.SetParent(optionObjects.transform, true);
+            opt1Object.transform.SetParent(optionObjects.transform, true);
+            opt2Object.transform.SetParent(optionObjects.transform, true);
+            opt3Object.transform.SetParent(optionObjects.transform, true);
+            optFinishObject.transform.SetParent(optionObjects.transform, true);
             
-            _contactSender.CreateContactSender(opt1Object, null, 0, 0.01f, Vector3.zero, Vector3.zero,
-                new List<string> { "OCS_Option1" });
+            _contactSender.CreateContactSender(opt0Object, null, 0, 3f, Vector3.zero, Vector3.zero,
+                new List<string> { "OCS0" });
             
-            _contactSender.CreateContactSender(opt2Object, null, 0, 0.01f, Vector3.zero, Vector3.zero,
-                new List<string> { "OCS_Option2" });
+            _contactSender.CreateContactSender(opt1Object, null, 0, 3f, Vector3.zero, Vector3.zero,
+                new List<string> { "OCS1" });
             
-            _contactSender.CreateContactSender(opt3Object, null, 0, 0.01f, Vector3.zero, Vector3.zero,
-                new List<string> { "OCS_Option3" });
+            _contactSender.CreateContactSender(opt2Object, null, 0, 3f, Vector3.zero, Vector3.zero,
+                new List<string> { "OCS2" });
             
-            _contactSender.CreateContactSender(optFinishObject, null, 0, 0.01f, Vector3.zero, Vector3.zero,
-                new List<string> { "OCS_Finish" });
+            _contactSender.CreateContactSender(opt3Object, null, 0, 3f, Vector3.zero, Vector3.zero,
+                new List<string> { "OCS3" });
             
-            var optConstraint = optionObjects.AddComponent<ParentConstraint>();
-            optConstraint.AddSource(new ConstraintSource { sourceTransform = world.transform, weight = 1 });
-            optConstraint.SetTranslationOffset(0, new Vector3(0, 0, 0));
-            optConstraint.constraintActive = true;
+            _contactSender.CreateContactSender(optFinishObject, null, 0, 3f, Vector3.zero, Vector3.zero,
+                new List<string> { "OCSF" });
+            
+            // var optConstraint = optionObjects.AddComponent<ParentConstraint>();
+            // optConstraint.AddSource(new ConstraintSource { sourceTransform = world.transform, weight = 1 });
+            // optConstraint.SetTranslationOffset(0, new Vector3(0, 0, 0));
+            // optConstraint.constraintActive = true;
             
             return sendersObject;
         }
@@ -88,7 +93,7 @@ namespace ANGELWARE.AW_OCS
             
             var versionObject = new GameObject("OCS_VersionReceiver");
             
-            versionObject.transform.SetParent(receiversObject.transform, false);
+            versionObject.transform.SetParent(receiversObject.transform, true);
 
             _contactReceiver.CreateContactReceiver(versionObject, null, 0, 1, 0,
                 Vector3.zero, Vector3.zero, new List<string> { "OCS_V1" }, false, 
@@ -98,54 +103,60 @@ namespace ANGELWARE.AW_OCS
             
             optionObjects.transform.SetParent(receiversObject.transform, false);
             
-            var opt1Object = new GameObject("OCS_Option1Receiver");
-            var opt2Object = new GameObject("OCS_Option2Receiver");
-            var opt3Object = new GameObject("OCS_Option3Receiver");
-            var optFinishObject = new GameObject("OCS_FinishReceiver");
+            var opt0Object = new GameObject("OCS0_Receiver");
+            var opt1Object = new GameObject("OCS1_Receiver");
+            var opt2Object = new GameObject("OCS2_Receiver");
+            var opt3Object = new GameObject("OCS3_Receiver");
+            var optFinishObject = new GameObject("OCSF_Receiver");
             
-            opt1Object.transform.SetParent(optionObjects.transform, false);
-            opt2Object.transform.SetParent(optionObjects.transform, false);
-            opt3Object.transform.SetParent(optionObjects.transform, false);
-            optFinishObject.transform.SetParent(optionObjects.transform, false);
+            opt0Object.transform.SetParent(optionObjects.transform, true);
+            opt1Object.transform.SetParent(optionObjects.transform, true);
+            opt2Object.transform.SetParent(optionObjects.transform, true);
+            opt3Object.transform.SetParent(optionObjects.transform, true);
+            optFinishObject.transform.SetParent(optionObjects.transform, true);
+            
+            _contactReceiver.CreateContactReceiver(opt0Object, null, 0, 1, 0,
+                Vector3.zero, Vector3.zero, new List<string> { "OCS0" }, false, 
+                true, true, 0, "OCS/Bit/0");
             
             _contactReceiver.CreateContactReceiver(opt1Object, null, 0, 1, 0,
-                Vector3.zero, Vector3.zero, new List<string> { "OCS_Option1" }, false, 
-                true, true, 0, "OCS/Opt1");
+                Vector3.zero, Vector3.zero, new List<string> { "OCS1" }, false, 
+                true, true, 0, "OCS/Bit/1");
             
             _contactReceiver.CreateContactReceiver(opt2Object, null, 0, 1, 0,
-                Vector3.zero, Vector3.zero, new List<string> { "OCS_Option2" }, false, 
-                true, true, 0, "OCS/Opt2");
+                Vector3.zero, Vector3.zero, new List<string> { "OCS2" }, false, 
+                true, true, 0, "OCS/Bit/2");
             
             _contactReceiver.CreateContactReceiver(opt3Object, null, 0, 1, 0,
-                Vector3.zero, Vector3.zero, new List<string> { "OCS_Option3" }, false, 
-                true, true, 0, "OCS/Opt3");
+                Vector3.zero, Vector3.zero, new List<string> { "OCS3" }, false, 
+                true, true, 0, "OCS/Bit/3");
             
             _contactReceiver.CreateContactReceiver(optFinishObject, null, 0, 1, 0,
-                Vector3.zero, Vector3.zero, new List<string> { "OCS_Finish" }, false, 
-                true, true, 0, "OCS/Finish");
+                Vector3.zero, Vector3.zero, new List<string> { "OCSF" }, false, 
+                true, true, 0, "OCS/Event/Finish");
             
-            var optConstraint = optionObjects.AddComponent<ParentConstraint>();
-            optConstraint.AddSource(new ConstraintSource { sourceTransform = world.transform, weight = 1 });
-            optConstraint.SetTranslationOffset(0, new Vector3(0, 0, 0));
-            optConstraint.constraintActive = true;
+            // var optConstraint = optionObjects.AddComponent<ParentConstraint>();
+            // optConstraint.AddSource(new ConstraintSource { sourceTransform = world.transform, weight = 1 });
+            // optConstraint.SetTranslationOffset(0, new Vector3(0, 0, 0));
+            // optConstraint.constraintActive = true;
 
-            if (_component.nsfwTouch)
-            {
-                var nsfwTouchObject = new GameObject("OCS_NSFWTouch");
-                _contactReceiver.CreateContactReceiver(nsfwTouchObject, _component.nsfwTouchTransform, 0, _component.nsfwTouchRadius, 0,
-                    Vector3.zero, Vector3.zero, new List<string> { "Hand", "Finger" }, false, 
-                    true, true, 0, "OCS/NSFW/Touch");
-                nsfwTouchObject.transform.SetParent(_component.nsfwTouchTransform, false);
-            }
-            
-            if (_component.dickTouch)
-            {
-                var nsfwTouchObject = new GameObject("OCS_DickTouch");
-                _contactReceiver.CreateContactReceiver(nsfwTouchObject, _component.dickTouchTransform, 0, _component.dickTouchRadius, 0,
-                    Vector3.zero, Vector3.zero, new List<string> { "Hand", "Finger" }, false, 
-                    true, true, 0, "OCS/NSFW/TouchDick");
-                nsfwTouchObject.transform.SetParent(_component.dickTouchTransform, false);
-            }
+            // if (_component.nsfwTouch)
+            // {
+            //     var nsfwTouchObject = new GameObject("OCS_NSFWTouch");
+            //     _contactReceiver.CreateContactReceiver(nsfwTouchObject, _component.nsfwTouchTransform, 0, _component.nsfwTouchRadius, 0,
+            //         Vector3.zero, Vector3.zero, new List<string> { "Hand", "Finger" }, false, 
+            //         true, true, 0, "OCS/NSFW/Touch");
+            //     nsfwTouchObject.transform.SetParent(_component.nsfwTouchTransform, false);
+            // }
+            //
+            // if (_component.dickTouch)
+            // {
+            //     var nsfwTouchObject = new GameObject("OCS_DickTouch");
+            //     _contactReceiver.CreateContactReceiver(nsfwTouchObject, _component.dickTouchTransform, 0, _component.dickTouchRadius, 0,
+            //         Vector3.zero, Vector3.zero, new List<string> { "Hand", "Finger" }, false, 
+            //         true, true, 0, "OCS/NSFW/TouchDick");
+            //     nsfwTouchObject.transform.SetParent(_component.dickTouchTransform, false);
+            // }
 
             return receiversObject;
 
